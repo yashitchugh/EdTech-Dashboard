b57603b287b68cfbae589c025519398bb6131385
parth5012 <parthchawla5012@GMAIL.COM>
2025-11-26 12:59:57 +0530

    Designed the workflow in utils

--- utils/graphs.py
+++ utils/graphs.py
@@ -2,8 +2,7 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, List
 
 # from datetime import date,datetime
-from utils.llms import llm
-from utils.prompt import ats_prompt
+from utils.llms import get_ats_score,get_job_details,get_json_output,get_readiness_score,get_str_output
 
 
 class JobDetails(TypedDict):
@@ -26,3 +25,46 @@ class DashboardState(TypedDict):
     analysis_quote: List[str]
     readiness_score: float
     ats_score: float
+
+def ats(state:DashboardState):
+    content = state['resume_text']
+    ats = get_ats_score(content)
+    return {'ats':ats}
+def job_details(state:DashboardState):
+    desc = state['job_desc']
+    details = get_job_details(desc)
+    return {'job_details':details}
+def candidate_details(state:DashboardState):
+    content = state['resume_text']
+    details = get_json_output(content)
+    return {'candidate_details':details}
+def quote(state:DashboardState):
+    content = state['resume_text']
+    quotes = get_str_output(content)
+    return {'analysis_quote':quotes}
+def readiness_score(state:DashboardState):
+    content = state['resume_text']
+    score = get_readiness_score(content)
+    return {'readiness_score':score}
+
+
+graph = StateGraph(DashboardState)
+
+graph.add_node('ats',ats)
+graph.add_node('quote',quote)
+graph.add_node('readiness_score',readiness_score)
+graph.add_node('job_details',job_details)
+graph.add_node('candidate_details',candidate_details)
+
+graph.add_edge(START,'ats')
+graph.add_edge(START,'quote')
+graph.add_edge(START,'readiness_score')
+graph.add_edge(START,'job_details')
+graph.add_edge(START,'candidate_details')
+graph.add_edge('ats',END)
+graph.add_edge('quote',END)
+graph.add_edge('readiness_score',END)
+graph.add_edge('job_details',END)
+graph.add_edge('candidate_details',END)
+
+workflow = graph.compile()
